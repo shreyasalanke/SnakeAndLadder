a67b085e13781eef5aa0b5ed73564a4e5457195d
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-22 18:00:46 +0530

    ADDED: random function to get value between 1 to 6

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -6,6 +6,8 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		
 		int player1 = 0;
+		double rollDice =Math.floor(Math.random()*10)%7;
+		System.out.println("dice number is:"+ rollDice);
 		
 	}
 	
