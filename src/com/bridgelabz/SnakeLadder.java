679896226efa5a700079bc4d919f19af0ae0d555
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-23 20:48:32 +0530

    ADDED: player plays till he reaches 100

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -8,44 +8,36 @@ public class SnakeLadder
 	
 	
 	public static void main(String[] args)
+	
 	{
-		
 		int player1 = 0;
-		int player2 = 0;
-		int NO_PLAY = 0;
-		int LADDER = 1;
-		int SNAKE = 2;
-		int MOVE = 0;
-		double rollDice1 =Math.floor(Math.random()*10)%7;
-		double rollDice2 =Math.floor(Math.random()*10)%3;
 		
 		
+		int dice1 = (int)(Math.floor(Math.random()* 10)%7);
+		System.out.println("die1 value is:"+dice1);
+		int dice2 = (int)(Math.floor(Math.random()* 10)%3);
 		
-		if (rollDice2 == LADDER  )
-			
+		switch ((int)dice2)
 		{
-		  System.out.println("dice1 value is:"+rollDice1);
-		  MOVE = (int)(player1+ rollDice1);
-		  System.out.println("position of player1 is:"+ MOVE);
-		  
+		case 0:
+			System.out.println("no play");
+			player1 += 0;
+			break;
+		case 1:
+			System.out.println("Ladder");
+			player1 += dice1;
+			break;
+        
+		default:
+			System.out.println("Snake");
+			player1 -= dice1;
+			break;
 		}
-		
-		else if (rollDice2 == SNAKE)
-		{
-				
-				System.out.println("dice1 value is :"+rollDice1);
-			    MOVE =(int)(player1-rollDice1);
-			    System.out.println("postion of player is:"+ MOVE);
-			    
-		}
-		
-		else
-		{
-			
-		   System.out.println("player get no play");
-			
-	  	}
+		if (player1 < 0)
+			player1=0;
 	
-    }
 	
+		System.out.println("palyer position is :"+player1);
+	}
+ 
 }
