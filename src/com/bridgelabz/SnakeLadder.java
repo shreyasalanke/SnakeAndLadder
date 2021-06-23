a0432be7f8122563bf42a48a41576b4b2a18021d
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-23 07:26:04 +0530

    MODIFIED: added option to check whther player gets ladder , snake or no play

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -1,15 +1,51 @@
 package com.bridgelabz;
 
-public class SnakeLadder {
+import java.util.Random;
+
+
+public class SnakeLadder
+{
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		
 		int player1 = 0;
-		double rollDice =Math.floor(Math.random()*10)%7;
-		System.out.println("dice number is:"+ rollDice);
+		int player2 = 0;
+		int NO_PLAY = 0;
+		int LADDER = 1;
+		int SNAKE = 2;
+		int MOVE = 0;
+		double rollDice1 =Math.floor(Math.random()*10)%7;
+		double rollDice2 =Math.floor(Math.random()*10)%3;
 		
-	}
+		
+		
+		if (rollDice2 == LADDER  )
+			
+		{
+		  System.out.println("dice1 value is:"+rollDice1);
+		  MOVE = (int)(player1+ rollDice1);
+		  System.out.println("position of player1 is:"+ MOVE);
+		  
+		}
+		
+		else if (rollDice2 == SNAKE)
+		{
+				
+				System.out.println("dice1 value is :"+rollDice1);
+			    MOVE =(int)(player1-rollDice1);
+			    System.out.println("postion of player is:"+ MOVE);
+			    
+		}
+		
+		else
+		{
+			
+		   System.out.println("player get no play");
+			
+	  	}
 	
-
+    }
+	
 }
